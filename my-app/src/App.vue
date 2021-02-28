9aa4c79f6a7ea31d8fc0e7c3882c079186069ee1
Kilbane <kilbanen@tcd.ie>
2021-02-28 20:10:11 +0000

    Selecting an image displays its information

--- my-app/src/App.vue
+++ my-app/src/App.vue
@@ -1,10 +1,19 @@
 <template>
   <div id="app">
-    <select name="Images">
-      <option v-for="(image, index) in images" :key="index">
-        {{image.aliases[0].name}}
+    <select @change="changeSelectedImage($event)">
+      <option>Select Image</option>
+      <option v-for="(image, index) in images" :value="JSON.stringify(image)" :key="index">
+        {{ image.aliases[0].name }}
       </option>
     </select>
+    <div>Name: {{ selectedImage.aliases[0].name }}</div>
+    <div>Fingerprint: {{ selectedImage.fingerprint }}</div>
+    <div>Architecture: {{ selectedImage.properties.architecture }}</div>
+    <div>Description: {{ selectedImage.properties.description }}</div>
+    <div>Operating System: {{ selectedImage.properties.os }}</div>
+    <div>Serial: {{ selectedImage.properties.serial }}</div>
+    <div>Type: {{ selectedImage.properties.type }}</div>
+    <div>Size: {{ selectedImage.size }}</div>
   </div>
 </template>
 
@@ -14,7 +23,25 @@ export default {
   name: "App",
   data() {
     return {
-      images: {}
+      images: {},
+      selectedImage: {
+        "aliases": [
+          {
+            "name": "",
+            "description": ""
+          }
+        ],
+        "fingerprint": "",
+        "properties": {
+          "architecture": "",
+          "description": "",
+          "os": "",
+          "release": "",
+          "serial": "",
+          "type": ""
+        },
+        "size": ""
+      }
     }
   },
   beforeMount(){
@@ -25,7 +52,10 @@ export default {
       const res = await fetch('https://webspaced.netsoc.ie/v1/images');
       const images = await res.json();
       this.images = images;
+    },
+    changeSelectedImage(event) {
+      this.selectedImage = JSON.parse(event.target.options[event.target.options.selectedIndex].value)
     }
   }
-};
+}
 </script>

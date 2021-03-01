6916d84285491f9b5ec9f8466e9e5c9cb7ff2e47
jissj9 <jjojoseph12@gmail.com>
2021-03-01 17:14:00 +0000

    Added a navigation bar

--- my-app/src/App.vue
+++ my-app/src/App.vue
@@ -1,26 +1,42 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <header>
+        <Navbar/>
+    </header>
+    
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Navbar from './components/NavigationBar';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    Navbar
+  },
+  
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+<style >
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box
 }
+
+body{
+  font-family: 'montserrat', sans-serif;
+
+}
+
+
+header{
+  width: 100vw;
+  background-color:rgb(57, 0, 190);
+  padding:25px;
+}
+
 </style>

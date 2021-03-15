94c42934b3efcb437cd33afc72b2a4c10987bd03
tedski999 <tedjohnsonjs@gmail.com>
2021-03-15 23:51:01 +0000

    Added method to set and verify JWT
    API.js setAuthToken() can be used to set the authentication token
    Non-empty tokens will be verified against the API end-point /users/self/token
    Cleaned up and updated API.js fetch() to use the new authToken

--- my-app/src/API.js
+++ my-app/src/API.js
@@ -1,22 +1,35 @@
 const https = require('https')
 
+// TODO: check authToken works globally
+let authToken = ''
+
 export const WEBSPACED_API_URL = 'webspaced.netsoc.ie/v1'
 export const IAM_API_URL = 'iam.netsoc.ie/v1'
 
-// TODO: (global?) jwt authentication
-// TODO: better error handling
-// TODO: POST body
+// Set and verify the new token if not empty
+export function setAuthToken (newToken) {
+  const oldToken = authToken
+  authToken = newToken
+  if (authToken.length !== 0) {
+    fetch(IAM_API_URL + '/users/self/token').catch(authToken = oldToken) // TODO: is this correct?
+  }
+}
 
+// General method for making calls to the Netsoc API
+// TODO: do we need custom request headers?
 export function fetch (url, method = 'GET', body = null) {
   return new Promise((resolve, reject) => {
-    const host = url.substring(0, url.indexOf('/'))
-    const path = url.substring(url.indexOf('/'))
+    // Request options
     const options = {
-      host: host,
-      path: path,
+      host: url.substring(0, url.indexOf('/')),
+      path: url.substring(url.indexOf('/')),
       method: method,
-      headers: {
-      }
+      headers: {}
+    }
+
+    // Authenticate with JWT if set
+    if (authToken.length !== 0) {
+      options.headers.Authorization = 'Bearer ' + authToken
     }
 
     // Create the request
@@ -34,10 +47,14 @@ export function fetch (url, method = 'GET', body = null) {
 
       // Parse and resolve successful on end
       res.on('end', () => {
-        try {
-          resolve(JSON.parse(data))
-        } catch (e) {
-          reject(e)
+        if (data.length === 0) {
+          resolve()
+        } else {
+          try {
+            resolve(JSON.parse(data))
+          } catch (e) {
+            reject(e)
+          }
         }
       })
     })

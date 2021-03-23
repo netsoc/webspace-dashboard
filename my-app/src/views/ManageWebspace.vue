fd4276dd1334384fbfdb20383cf41519224c8276
Maria Cairns <cairnsm@tcd.ie>
2021-03-23 18:17:35 +0000

    Update ManageWebspace.vue
    Doing an initial push, need to fix styling and the error

--- my-app/src/views/ManageWebspace.vue
+++ my-app/src/views/ManageWebspace.vue
@@ -1,13 +1,46 @@
 <template>
-  <div class="ManageWebspace">
-    <p>Manage Webspace</p>
+  <div>
+    <div class="ManageWebspace">
+      <p>Manage Webspace</p>
+    </div>
+    <div class="buttons">
+      <button @click="destroyWebspace">
+        Destroy Webspace
+      </button>
+      <button @click="resetWebspace">
+        Reset Webspace
+      </button>
+    </div>
   </div>
 </template>
 
 <script>
-
+import * as API from '@/API.js'
 export default {
-
+  name: 'ManageWebspace',
+  data () {
+    return {}
+  },
+  methods: {
+    async destroyWebspace () {
+      try {
+        // add confirmation page "are you sure?" etc
+        confirm('Are you sure you want to destroy this webspace?')
+        // delete action
+        // need to fix it
+        this.destroyWebspace = await API.fetch(
+          API.WEBSPACED_API_URL + '/webspace/self'
+        )
+        // notification that it has deleted
+        alert('The webspace has been successfully destroyed')
+      } catch (err) {
+        alert('Unable to destroy webspace' + err.message)
+      }
+    },
+    resetWebspace () {
+      alert('TODO')
+    }
+  }
 }
 </script>
 
